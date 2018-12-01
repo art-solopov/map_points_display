b48a74344b838fa69d9e39ba652f2ce3be3fe2ca
Artemiy Solopov <art-solopov@yandex.ru>
2018-12-01 22:13:02 +0300

    Data rendering

--- src/map_points_display/routes.clj
+++ src/map_points_display/routes.clj
@@ -1,7 +1,11 @@
 (ns map-points-display.routes
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [map-points-display.views :as views]))
+            [map-points-display.views :as views]
+            [map-points-display.data :as data]))
 
 (defroutes app
-  (GET "/" [] (views/home {:message "Map app"})))
+  (GET "/" []
+       (let [all-d (data/load-data data/default-data)
+             groups (group-by :type all-d)]
+         (views/home {:groups groups :message "Map app"}))))
--- src/map_points_display/views.clj
+++ src/map_points_display/views.clj
@@ -1,6 +1,23 @@
 (ns map-points-display.views
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [clojure.string :as s]
+            [net.cgrand.enlive-html :as html]))
+
+(html/defsnippet group-item "templates/index.html"
+  [:section.group :ul.items :> :li]
+  [item]
+  [:li] (html/do->
+         (html/content (:name item))
+         (html/set-attr :data-lat (:lat item))
+         (html/set-attr :data-lon (:lon item))))
+
+(html/defsnippet group "templates/index.html"
+  [:section.group]
+  [name items]
+  [html/root] (html/set-attr :data-name name)
+  [:h2.group-type] (html/content (s/capitalize name))
+  [:ul.items] (html/content (map group-item items)))
 
 (html/deftemplate home "templates/index.html"
   [ctxt]
-  [:#app :h1] (html/content (:message ctxt)))
+  [:#app :h1] (html/content (:message ctxt))
+  [:#app :.places] (html/content (map #(apply group %) (:groups ctxt))))
