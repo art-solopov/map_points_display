4785c411805e738925c238fc7540b38aed58f8d1
Artemiy Solopov <art-solopov@yandex.ru>
2018-12-12 11:37:30 +0300

    Preparing for multiple files list

--- src/map_points_display/routes.clj
+++ src/map_points_display/routes.clj
@@ -8,10 +8,10 @@
 (defroutes app
   (GET "/" []
        (let [groups (data/load-data data/default-data)]
-         (views/home {:groups groups :message "Map app"})))
+         (views/show-file {:groups groups :message "Map app"})))
   (GET "/:data-file" [data-file]
        (let [groups (data/load-data (str data-file ".csv"))]
-         (views/home {:groups groups :message (str "Map app: " data-file)})))
+         (views/show-file {:groups groups :message (str "Map app: " data-file)})))
   (GET "/templates/:filename" [filename]
        (resource-response filename {:root "templates"}))
   (route/not-found "Not found"))
--- src/map_points_display/views.clj
+++ src/map_points_display/views.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as s]
             [net.cgrand.enlive-html :as html]))
 
-(html/defsnippet group-item "templates/index.html"
+(html/defsnippet group-item "templates/show.html"
   [:section.group :ul.items :> :li]
   [item]
   [:li] (html/do->
@@ -12,14 +12,14 @@
   [:.item-name] (html/content (:name item))
   [:.item-address] (html/content (:address item)))
 
-(html/defsnippet group "templates/index.html"
+(html/defsnippet group "templates/show.html"
   [:section.group]
   [name items]
   [html/root] (html/set-attr :data-name name)
   [:h2.group-type] (html/content (s/capitalize name))
   [:ul.items] (html/content (map group-item items)))
 
-(html/deftemplate home "templates/index.html"
+(html/deftemplate show-file "templates/show.html"
   [ctxt]
   [:#app :h1] (html/content (:message ctxt))
   [:#app :.places] (html/content (map #(apply group %) (:groups ctxt))))
