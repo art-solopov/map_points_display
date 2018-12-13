ad18fd15e9535c3ac56a3657cbd5a81126dfbea6
Artemiy Solopov <art-solopov@yandex.ru>
2018-12-13 21:02:21 +0300

    Some tinkerings

--- src/map_points_display/routes.clj
+++ src/map_points_display/routes.clj
@@ -1,5 +1,6 @@
 (ns map-points-display.routes
-  (:require [compojure.core :refer :all]
+  (:require [clojure.string :as s]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.util.response :refer [resource-response]]
             [map-points-display.views :as views]
@@ -11,7 +12,7 @@
          (views/index-file {:files files})))
   (GET "/data/:data-file" [data-file]
        (let [groups (data/load-data (str data-file ".csv"))]
-         (views/show-file {:groups groups :message (str "Map app: " data-file)})))
+         (views/show-file {:groups groups :message (s/capitalize data-file)})))
   (GET "/templates/:filename" [filename]
        (resource-response filename {:root "templates"}))
   (route/not-found "Not found"))
