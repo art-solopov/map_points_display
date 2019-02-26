2e5c0062503f4566c4e64285b3366fb398e361c5
Artemiy Solopov <art-solopov@yandex.ru>
2019-02-26 13:22:13 +0300

    Added the click-pan handler

--- src-cljs/map_points_display/core.cljs
+++ src-cljs/map_points_display/core.cljs
@@ -1,7 +1,8 @@
 (ns map-points-display.core
   (:require [map-points-display.data-points :as dp]
             [map-points-display.ext.leaflet :as leaflet]
-            [map-points-display.events :refer [make-mouse-events-processor]]))
+            [map-points-display.events :refer
+             [make-mouse-events-processor make-click-processor]]))
 
 (enable-console-print!)
 
@@ -19,13 +20,15 @@
   (let [ds (map :data @data-points)
         center-lat (avg-coord ds :lat)
         center-lon (avg-coord ds :lon)
-        entlv-proc (make-mouse-events-processor @data-points)]
+        entlv-proc (make-mouse-events-processor @data-points)
+        click-proc (make-click-processor the-map)]
     (reset! the-map (leaflet/make-map center-lat center-lon 13))
     (doseq [point @data-points]
       (.addTo (:marker point) @the-map)
       (let [el (:el point)]
         (set! (.. el -dataset -elid) (:uuid point))
         (.addEventListener el "mouseenter" entlv-proc)
-        (.addEventListener el "mouseleave" entlv-proc)))))
+        (.addEventListener el "mouseleave" entlv-proc)
+        (.addEventListener (.querySelector el "a") "click" click-proc)))))
 
 (-main)
--- src-cljs/map_points_display/events.cljs
+++ src-cljs/map_points_display/events.cljs
@@ -2,16 +2,16 @@
   (:require [map-points-display.markers :refer [stroke-color-default
                                                 stroke-color-hover]]))
 
-(defn- event-info [event data-points]
-  (let [el (.-target event)
-        el-id (uuid (.. el -dataset -elid))
+(defn- event-info [element data-points]
+  (let [el-id (uuid (.. element -dataset -elid))
         data-pt (->> data-points (filter #(= (:uuid %) el-id)) first)]
-    {:el el :el-id el-id :data-pt data-pt}))
+    {:el-id el-id :data-pt data-pt}))
 
 (defn make-mouse-events-processor [data-points]
   (fn [ev]
-    (let [evinfo (event-info ev data-points)
-          {:keys [el el-id data-pt]} evinfo
+    (let [el (.-target ev)
+          evinfo (event-info el data-points)
+          {:keys [el-id data-pt]} evinfo
           {:keys [marker data]} data-pt
           {:keys [category]} data
           style (case (.-type ev)
@@ -20,3 +20,12 @@
       (.setStyle marker (clj->js style)))
     )
   )
+
+(defn make-click-processor [map-atom]
+  (fn [ev]
+    (let [t (.-target ev)
+          el (.-parentElement t)
+          lat (js/Number (.. el -dataset -lat))
+          lon (js/Number (.. el -dataset -lon))]
+      (.preventDefault ev)
+      (.panTo @map-atom (array lat lon)))))
