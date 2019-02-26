3f4e50cc926029314b572eb98ef58344fdcd8c94
Artemiy Solopov <art-solopov@yandex.ru>
2019-02-26 12:28:44 +0300

    Minor refactoring

--- src-cljs/map_points_display/core.cljs
+++ src-cljs/map_points_display/core.cljs
@@ -8,14 +8,17 @@
 (def the-map (atom nil))
 (def data-points (atom nil))
 
+(defn avg-coord [data-points coord]
+  (let [sum (->> data-points (map coord) (reduce +))
+        ct (count data-points)]
+    (/ sum ct)))
+
 (defn -main []
   (reset! data-points (dp/read-data))
   (-> js/document (.getElementById "map") (.-innerHTML) (set! ""))
   (let [ds (map :data @data-points)
-        data-count (count ds)
-        ; TODO: replace with transducers?
-        center-lat (->> ds (map :lat) (map #(/ % data-count)) (reduce +))
-        center-lon (->> ds (map :lon) (map #(/ % data-count)) (reduce +))
+        center-lat (avg-coord ds :lat)
+        center-lon (avg-coord ds :lon)
         entlv-proc (make-mouse-events-processor @data-points)]
     (reset! the-map (leaflet/make-map center-lat center-lon 13))
     (doseq [point @data-points]
