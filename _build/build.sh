c9ed8f90b177b73d713aa5aa2f949db6f3a749bf
Artemiy Solopov <art-solopov@yandex.ru>
2019-03-10 13:55:03 +0300

    Made the build script

---
+++ _build/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+lein figwheel-prod && gulp css && lein ring uberwar || exit 1
+warfile=`ls -1 -t target/uberjar/*.war | head -1`
+cp $warfile _build/map_points_display.war
+cd _build
+docker build -t map_points_display .
