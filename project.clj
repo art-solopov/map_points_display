0bebeb7dd5b852f657b723e54ee4bf2bb3ceb1a2
Artemiy Solopov <art-solopov@yandex.ru>
2019-04-03 11:35:21 +0300

    Preparing the 0.1 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject map_points_display "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject map_points_display "0.1.0"
+  :description "A simple web app that displays predefined POI on the map"
+  :url "https://github.com/art-solopov/map_points_display"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
