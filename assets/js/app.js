c04e49516a9f6a6c0c7e22e82d052a63ee37a8e5
Artemiy Solopov <art-solopov@yandex.ru>
2019-04-08 00:12:03 +0300

    Remaking the app in pure JS, part 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,6 +36,8 @@ const BASE_MARKER_OPTS = {
     weight: 2
 }
 
+const MARKER_HOVER_COLOR = "#be005f"
+
 function makeMarker({name, lat, lon, category}) {
     let options = Object.assign({fillColor: MARKER_COLORS[category]},
                                 BASE_MARKER_OPTS)
@@ -43,15 +45,44 @@ function makeMarker({name, lat, lon, category}) {
     return L.circleMarker([lat, lon], options).bindPopup(`${name} [${category}]`)
 }
 
+function genRandomId() {
+    let a = Math.ceil(Math.random() * 1000)
+    let b = Math.ceil(Math.random() * 10000)
+    let c = Math.ceil(Math.random() * 100)
+
+    return `${a}-${b}-${c}`
+}
+
+function makeMouseEventProcessor(dataPoints) {
+    return event => {
+        let { target, type: evtype } = event;
+        let { elid } = target.dataset;
+        let dp = dataPoints.find(e => e.id === elid)
+
+        let style = {};
+        if(evtype == 'mouseenter') { style.color = MARKER_HOVER_COLOR }
+        else { style.color = "black" }
+        dp.marker.setStyle(style)
+    }
+}
+
 function main() {
     let map = L.map('map').setView([43.766, 11.233], 15)
     L.tileLayer(MAP_URL, {attribution: MAP_ATTRIBUTION}).addTo(map)
 
     dataPoints = getDataPoints()
-    dataPoints.forEach((el) => {
-        el.marker = makeMarker(el.data)
+    let moev = makeMouseEventProcessor(dataPoints)
 
-        el.marker.addTo(map)
+    dataPoints.forEach((dp) => {
+        dp.marker = makeMarker(dp.data)
+        dp.marker.addTo(map)
+
+        // TODO: replace with an actual id
+        dp.id = genRandomId()
+        dp.el.dataset.elid = dp.id
+
+        dp.el.addEventListener('mouseenter', moev)
+        dp.el.addEventListener('mouseleave', moev)
     })
 }
 
