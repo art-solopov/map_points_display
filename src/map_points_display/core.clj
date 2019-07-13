6de24d553d86cfec3f1c7725581773b5db39a3df
Artemiy Solopov <art-solopov@yandex.ru>
2019-07-13 16:57:58 +0300

    Added the Referrer-Policy response header

--- src/map_points_display/core.clj
+++ src/map_points_display/core.clj
@@ -1,5 +1,6 @@
 (ns map-points-display.core
   (:require [ring.adapter.jetty :as rj]
+            [ring.util.response :refer [header]]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.content-type :refer [wrap-content-type]]
@@ -17,8 +18,15 @@
   []
   (data-p/shutdown))
 
+(defn- wrap-set-ref-header
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (header response "Referrer-Policy" "origin-when-cross-origin"))))
+
 (def handler
   (-> routes/app
+      wrap-set-ref-header
       (wrap-resource "public")
       wrap-content-type
       wrap-not-modified))
