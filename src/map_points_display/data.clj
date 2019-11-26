87dc2f543a1bdea21feddb94006162527fede81b
Artemiy Solopov <art-solopov@yandex.ru>
2019-11-27 01:45:04 +0300

    Started migrating data functions to Postgres

--- src/map_points_display/data.clj
+++ src/map_points_display/data.clj
@@ -4,97 +4,41 @@
             [clojure.java.io :as io]
             [clojure.data.json :as json]
             [clojure.tools.logging :as log]
+            [jdbc.core :as jdbc]
             [environ.core :refer [env]]
             [clj-http.client :as http]
             [taoensso.carmine :as car :refer [wcar]]
             [map-points-display.config :refer [config secrets]]
             [map-points-display.data.fetch :refer [fetch-airtable-data]]
-            [map-points-display.utils :refer [path-join]]))
+            [map-points-display.utils :refer [path-join]]
+            [map-points-display.data.config :refer [data-source]]))
 
 (defmacro wcar* [& body]
   `(wcar (@config :redis) ~@body))
 
-;; Table list
+;; TODO: update interface
 
-(def ^:private tables-list-key "map-points:tables-list")
-(def ^:private tables-list-meta-key "map-points:_meta_tables-list")
+;; Table list
 
 (defn tables-list
   []
-  (wcar* (car/get tables-list-key)))
-
-(defn fetch-tables-list
-  []
-  (let [{etag :etag} (wcar* (car/get tables-list-meta-key))]
-    (if-let [atb-data (fetch-airtable-data "/Locations"
-                                           {:query-params {:view "Only Show" :fields ["Name"]}
-                                            :etag etag})]
-      (let [{data :data :as all} atb-data
-            names (->> data :records (map #(->> % :fields :Name)) (into []))]
-        (merge all {:data names})))))
-
-(defn update-tables-list
-  []
-  (if-let [{:keys [data meta]} (fetch-tables-list)]
-    (wcar* (car/set tables-list-key data)
-           (car/set tables-list-meta-key meta))))
+  (with-open [conn (jdbc/connection data-source)]
+    (jdbc/fetch conn "SELECT * FROM trips")))
 
 ;; Tables
 
-(defn- table-key
-  [table]
-  (str "map-points:table:" table))
-
-(defn- meta-key
-  [table]
-  (str "map-points:_meta:" table))
-
-(defn read-table
-  [table]
-  (wcar* (car/get (table-key table))))
-
-(defn read-table-meta
-  [table]
-  (wcar* (car/get (meta-key table))))
-
 (defn load-data
   [table-name]
-  (let [raw-data (read-table table-name)]
-    (group-by :type raw-data)))
+  (with-open [conn (jdbc/connection data-source)]
+    (let [q (str "SELECT points.* FROM points\n"
+                 "JOIN trips t on points.trip_id = t.id\n"
+                 "WHERE t.name = ?")]
+      (jdbc/fetch conn [q table-name]))))
 
 (defn load-poi
   [table-name id]
-  (let [raw-data (read-table table-name)]
-    (->> raw-data
-         (filter #(= (:id %) id))
-         first)))
+  (with-open [conn (jdbc/connection data-source)]
+    (-> conn
+        (jdbc/fetch ["SELECT * FROM points WHERE id = ?" id])
+        first)))
 
-(defn- table-request-headers
-  [table-name]
-  (let [{api-key :airtable-api-key} @secrets
-        hdr {"Authorization" (str "Bearer " api-key)}]
-    (if-let [{etag :etag} (wcar* (car/get (meta-key table-name)))]
-      (merge hdr {"If-None-Match" etag})
-      hdr)))
-
-(defn- postprocess-row
-  [data-row]
-  (let [{:keys [id fields]} data-row]
-    (merge fields {:id id})))
-
-(defn fetch-table-data
-  [table-name]
-  (let [{etag :etag} (read-table-meta table-name)
-        formula (format "AND(location=\"%s\", name, lat, lon, type)" table-name)]
-    (if-let [table-data (fetch-airtable-data "/PoI"
-                                             {:query-params {:filterByFormula formula}
-                                              :etag etag})]
-      (let [{data :data :as all} table-data
-            records (->> data :records (map postprocess-row) (into []))]
-        (merge all {:data records})))))
-
-(defn update-table-data
-  [table]
-  (when-let [{:keys [data meta]} (fetch-table-data table)]
-    (wcar* (car/set (table-key table) data)
-           (car/set (meta-key table) meta))))
--- src/map_points_display/data/periodic.clj
+++ src/map_points_display/data/periodic.clj
@@ -1,19 +1,20 @@
 (ns map-points-display.data.periodic
   (:require [clojure.tools.logging :as log]
             [mount.core :refer [defstate]]
-            [map-points-display.data :refer [tables-list update-tables-list update-table-data]])
+            ;; [map-points-display.data :refer [tables-list update-tables-list update-table-data]])
   (:import [java.util.concurrent ScheduledThreadPoolExecutor TimeUnit]))
 
 (defonce executor (atom nil))
 
 (defn update-tables
   []
-  (log/info "Updating tables list")
-  (update-tables-list)
-  (doseq [table (tables-list)]
-    (log/info (str "Updating table " table))
-    (update-table-data table)
-    (Thread/sleep 1000)))
+  ;; (log/info "Updating tables list")
+  ;; (update-tables-list)
+  ;; (doseq [table (tables-list)]
+  ;;   (log/info (str "Updating table " table))
+  ;;   (update-table-data table)
+  ;;   (Thread/sleep 1000))
+  )
 
 (defn init
   []
